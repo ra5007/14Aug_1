dbd9c1fcc21b93099949440d52643d905ea20192
Shaik Md N Rasool <rasool.sk@hotmail.com>
2020-08-14 11:18:39 +0530

    Updates

---
+++ precache-manifest.c09dcfff9d7a48ec0b23512ca0b4b2f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ca2df5900f37329d40bff77cce5dfd",
+    "url": "/14Aug_2/index.html"
+  },
+  {
+    "revision": "9167129284c9bd0b9bb6",
+    "url": "/14Aug_2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f77c974b53e93f7efea5",
+    "url": "/14Aug_2/static/js/2.cdb9a5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/14Aug_2/static/js/2.cdb9a5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9167129284c9bd0b9bb6",
+    "url": "/14Aug_2/static/js/main.8fc14efb.chunk.js"
+  },
+  {
+    "revision": "e7632377505cf9eae31d",
+    "url": "/14Aug_2/static/js/runtime-main.04df3900.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/14Aug_2/static/media/logo.5d5d9eef.svg"
+  }
+]);
